9e4bcfa766e0a3215257c34906cc450006221c13
Alex Kuczala <kookzala@gmail.com>
2022-04-30 16:18:50 -0400

    type comments

--- src/constraineddynamics/types.py
+++ src/constraineddynamics/types.py
@@ -2,10 +2,11 @@ from typing import NewType
 
 from numpy import ndarray
 
-SimVec = NewType("SimVec", ndarray)
-SpaceVec = NewType("SpaceVec", ndarray)
-SpaceVecs = NewType("SpaceVecs", ndarray)
-QVec = NewType("QVec", ndarray)
-ParticleVec = NewType("ParticleVec", ndarray)
+SimVec = NewType("SimVec", ndarray) # length 2 * n_particles * dim
+QVec = NewType("QVec", ndarray) # length n_particles * dim
+SpaceVec = NewType("SpaceVec", ndarray) # length dim
+ParticleVec = NewType("ParticleVec", ndarray) # length n_particles
+ConstraintVec = NewType("ConstraintVec", ndarray) # length len(constraints)
 
-QMatrix = NewType("QMatrix", ndarray)
+SpaceVecs = NewType("SpaceVecs", ndarray) # n_particles x dim
+QMatrix = NewType("QMatrix", ndarray) # (n_particles * dim) x (n_particles * dim)
